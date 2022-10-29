5d9069c391008e11957ba2e32033325b17fabca1
learningmonster <learningmonster@users.noreply.github.com>
2022-10-29 08:14:27 -0700

    Linux Python debug fixes for TestCases folder

--- LeetCode_Python/Utilities/FileOperationsModule.py
+++ LeetCode_Python/Utilities/FileOperationsModule.py
@@ -4,8 +4,10 @@ class FileOperations(object):
     """description of class"""
     
     # Under Linux
-    TestCasesFolder = "../TestCases/"
-    # TestCases/0014_LongestCommonPrefix.txt
+    # When running WITHOUT debugging
+    # TestCasesFolder = "../TestCases/"
+    # When running WITH debugging
+    TestCasesFolder = "TestCases/"
     
     # Under Windows
     # TestCasesFolder = "D:\\Projects\\ProgrammingChallenges\\LeetCode\\TestCases\\"
@@ -15,31 +17,7 @@ class FileOperations(object):
         print(f"__name__11 = {__name__}\n")
 
     #// TODO: Set folder location as a configurable setting
-        #static string TestCasesFolder = @"D:\Projects\ProgrammingChallenges\LeetCode\TestCases\";
-        #self.TestCasesFolder = "D:\\Projects\\ProgrammingChallenges\\LeetCode\\TestCases\\"
 
-        #TestCasesFolder = "D:\\Projects\\ProgrammingChallenges\\LeetCode\\TestCases\\"
-
-        #// TestCases location under Linux:
-        #// static string TestCasesFolder = "~/Learning/LeetCode/LeetCode/TestCases/";
-
-        #// TestCases location under Windows:
-        #// static string TestCasesFolder = @"D:\Projects\ProgrammingChallenges\LeetCode\TestCases\";
-
-        #internal FileOperations()
-        #{
-
-        #}
-
-        #public static string[] ReadLinesInFile(string inputFile)
-        #{
-        #    string FullFilePath = TestCasesFolder + inputFile;
-        #    // Read each line of the file into a string array. Each element
-        #    // of the array is one line of the file.
-        #    string[] lines = System.IO.File.ReadAllLines(FullFilePath);
-
-        #    return lines;
-        #}
 
     #def ReadLinesInFile(self, inputFile):
     #@staticmethod
@@ -54,10 +32,16 @@ class FileOperations(object):
         #FullFilePath = self.TestCasesFolder + inputFile;
         FullFilePath = FileOperations.TestCasesFolder + inputFile;
         print(f"FullFilePath = {FullFilePath}")
-            
+        
+        cwd = os.getcwd()
+        print(f"cwd = {cwd}")
+        
+        
         if not os.path.isfile(FullFilePath):
             print('File does not exist.')
-            lines = None
+            with open(FullFilePath, 'r', encoding='utf8') as FileReader:
+                lines = FileReader.readlines()
+            # lines = None
         else:
             #Read each line of the file into a string array. Each element
             #of the array is one line of the file.
